8dc2dff38956627adc66f8456cd3e371f33dcc8d
Leigh <leightheduck@gmail.com>
2020-04-19 18:23:06 +1000

    A2DP SAAB Receiver Prototype Works!

--- esp32_a2dp_recv/src/main.cpp
+++ esp32_a2dp_recv/src/main.cpp
@@ -2,46 +2,52 @@
 #include <SPI.h>
 #include "esp32_bt_music_receiver.h"
 #include <mcp2515.h>
+#include <esp_wifi.h>
+#include "soc/rtc.h"
 
 BlootoothA2DSink a2d_sink;
 
+#define USE_CAN
 
-struct can_frame canMsg;
-MCP2515 mcp2515(5);
+#ifdef USE_CAN
+  struct can_frame canMsg;
+  MCP2515 mcp2515(5);
+#endif
+
+// I2C Pins
+// ws_io_num => GPIO 25,
+// data_out_num => GPIO 22
+// bck_io_num => GPIO 26,
 
 void setup() {
   Serial.begin(115200);
-  SPI.begin();
+  Serial.println("BOOTED!");
 
+  // Turn off wifi
+  esp_wifi_set_mode(WIFI_MODE_NULL);
+  // esp_wifi_stop();
+
+#ifdef USE_CAN
+  SPI.begin();
   mcp2515.reset();
   mcp2515.setBitrate(CAN_33KBPS,CAN_CLOCK::MCP_8MHZ);
   mcp2515.setNormalMode();
-  // mcp2515.setLoopbackMode();
+#endif
 
-  // SETUP EXTERNAL DAC OUTPUT
-  static const i2s_config_t i2s_config = {
-      .mode = (i2s_mode_t) (I2S_MODE_MASTER | I2S_MODE_TX | I2S_MODE_DAC_BUILT_IN),
-      .sample_rate = 44100, // corrected by info from bluetooth
-      .bits_per_sample = (i2s_bits_per_sample_t) 16, /* the DAC module will only take the 8bits from MSB */
-      .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
-      .communication_format = I2S_COMM_FORMAT_I2S_MSB,
-      .intr_alloc_flags = 0, // default interrupt priority
-      .dma_buf_count = 8,
-      .dma_buf_len = 64,
-      .use_apll = false
-  };
-
-  a2d_sink.set_i2s_config(i2s_config);  
-  a2d_sink.start("SAAB Bluetooth Devboard");  
+  // Fix the audio clock 
+  rtc_clk_apll_enable(1, 15, 8, 5, 6);
+  a2d_sink.start("SAAB");  
 }
 
-bool isPlaying = false;
+bool playingStateRequest = false;
+bool playingState = false;
 bool pressed = false;
 String lastContent = "";
 
 void loop() {
 
   // Read messages
+  #ifdef USE_CAN
   if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) {
     if(canMsg.can_id == 0x290)
     {
@@ -55,24 +61,26 @@ void loop() {
         case 0x5:
           Serial.println("NEXT");
           esp_avrc_ct_send_passthrough_cmd(10,ESP_AVRC_PT_CMD_FORWARD,ESP_AVRC_PT_CMD_STATE_PRESSED);
-          delay(100);
+          delay(50);
           //NEXT
           break;
         case 0x6:
           Serial.println("PREV");
           esp_avrc_ct_send_passthrough_cmd(11,ESP_AVRC_PT_CMD_BACKWARD,ESP_AVRC_PT_CMD_STATE_PRESSED);
-          delay(100);
+          delay(50);
           //PREV
           break;
         case 0x11:
           Serial.println("PLAY/PAUSE");
-          if(isPlaying)
+          if(playingStateRequest)
           {
             esp_avrc_ct_send_passthrough_cmd(12,ESP_AVRC_PT_CMD_PAUSE,ESP_AVRC_PT_CMD_STATE_PRESSED);
           }else{
             esp_avrc_ct_send_passthrough_cmd(13,ESP_AVRC_PT_CMD_PLAY,ESP_AVRC_PT_CMD_STATE_PRESSED);
           }
-          delay(100);
+          // Update the boolean
+          playingStateRequest = !playingStateRequest;
+          delay(50);
           //PLAY/PAUSE
           break;
       }    
@@ -82,7 +90,7 @@ void loop() {
   // If playing content
   if(a2d_sink.get_audio_state() == 2)
   {
-    isPlaying = true;
+    playingState = true;
     String currentContent = "";
     currentContent = a2d_sink.get_track_name() + " - " + a2d_sink.get_album_name() + " by " + a2d_sink.get_artist_name();
 
@@ -92,6 +100,7 @@ void loop() {
       Serial.println(currentContent);
     }
   }else{
-    isPlaying = false;
+    playingState = false;
   }
+  #endif
 }
