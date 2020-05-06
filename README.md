4c7a4a190e198c8098ffdd34b8e84cead94d5589
Leigh <leightheduck@gmail.com>
2020-05-06 18:53:21 +1000

    Updated README

--- README.md
+++ README.md
@@ -2,21 +2,18 @@
 A module to augment the Aux-In functionality of SAAB 93NG's (2002/3+), to add Bluetooth media playback and steering wheel media control support.
 
 # Project Layout
+- /saab_bluetooth_aux_kit
+  - This is the PCB which all the modules will solder onto, for easy assembly.
 - /93ng-blue-aux 
-  - This is the KiCad 5 project!
-- /esp32_a2dp_recv  
+  - This is the OLD KiCAD 5 project.
+- /esp32_a2dp_reciever  
   - PIO project, the software flashed to the ESP32 module to provide bluetooth audio -> I2S functionality.
-  - Also handles CAN bus messages.
+  - Also handles CAN bus messages, for steering wheel Play/Pause/Next/Previous
 
 # Project status
-~~Designing the board and ordering parts to prototype with.~~
 Bought the following modules and built a working prototype!
 - ESP32 NodeMCU Board
 - Adafruit I2S to Stereo DAC
-- MCP2515 CAN-bus to SPI interface module 
-### Current Issues
-- Sometimes glitchy noises in audio (usually only at very start of playback)
-- Play/pause needs double press on first start (logic issue)
-- Audio output is low, which means the car gets turned up high, which makes noisy audio.
-	- Resolve by increasing DAC resolution maybe?
-- DAC is only 16 bit. This is fine, if it were at the correct level.
+- MCP2515 CAN-bus to SPI interface module
+
+Currently writing a new stack using only the ESP-IDF SDK, to allow for simultaneous phone call handling etc.
--- esp32_a2dp_reciever/src/main.cpp
+++ esp32_a2dp_reciever/src/main.cpp
@@ -1,7 +1,9 @@
 #include <Arduino.h>
 #include <SPI.h>
-#include "esp32_bt_music_receiver.h"
 #include <mcp2515.h>
+
+#include "esp32_bt_music_receiver.h"
+
 #include <esp_wifi.h>
 #include "soc/rtc.h"
 #include <Wire.h>
@@ -10,66 +12,6 @@
 #include <Fonts/Picopixel.h>
 #include <Fonts/FreeMono12pt7b.h>
 
-// List of AVRC passthrough commands, hoping to find one that can trigger
-// ok google or SIRI input.
-// #define AVRC_ID_SELECT      0x00    /* select */
-// #define AVRC_ID_UP          0x01    /* up */
-// #define AVRC_ID_DOWN        0x02    /* down */
-// #define AVRC_ID_LEFT        0x03    /* left */
-// #define AVRC_ID_RIGHT       0x04    /* right */
-// #define AVRC_ID_RIGHT_UP    0x05    /* right-up */
-// #define AVRC_ID_RIGHT_DOWN  0x06    /* right-down */
-// #define AVRC_ID_LEFT_UP     0x07    /* left-up */
-// #define AVRC_ID_LEFT_DOWN   0x08    /* left-down */
-// #define AVRC_ID_ROOT_MENU   0x09    /* root menu */
-// #define AVRC_ID_SETUP_MENU  0x0A    /* setup menu */
-// #define AVRC_ID_CONT_MENU   0x0B    /* contents menu */
-// #define AVRC_ID_FAV_MENU    0x0C    /* favorite menu */
-// #define AVRC_ID_EXIT        0x0D    /* exit */
-// #define AVRC_ID_0           0x20    /* 0 */
-// #define AVRC_ID_1           0x21    /* 1 */
-// #define AVRC_ID_2           0x22    /* 2 */
-// #define AVRC_ID_3           0x23    /* 3 */
-// #define AVRC_ID_4           0x24    /* 4 */
-// #define AVRC_ID_5           0x25    /* 5 */
-// #define AVRC_ID_6           0x26    /* 6 */
-// #define AVRC_ID_7           0x27    /* 7 */
-// #define AVRC_ID_8           0x28    /* 8 */
-// #define AVRC_ID_9           0x29    /* 9 */
-// #define AVRC_ID_DOT         0x2A    /* dot */
-// #define AVRC_ID_ENTER       0x2B    /* enter */
-// #define AVRC_ID_CLEAR       0x2C    /* clear */
-// #define AVRC_ID_CHAN_UP     0x30    /* channel up */
-// #define AVRC_ID_CHAN_DOWN   0x31    /* channel down */
-// #define AVRC_ID_PREV_CHAN   0x32    /* previous channel */
-// #define AVRC_ID_SOUND_SEL   0x33    /* sound select */
-// #define AVRC_ID_INPUT_SEL   0x34    /* input select */
-// #define AVRC_ID_DISP_INFO   0x35    /* display information */
-// #define AVRC_ID_HELP        0x36    /* help */
-// #define AVRC_ID_PAGE_UP     0x37    /* page up */
-// #define AVRC_ID_PAGE_DOWN   0x38    /* page down */
-// #define AVRC_ID_POWER       0x40    /* power */
-// #define AVRC_ID_VOL_UP      0x41    /* volume up */
-// #define AVRC_ID_VOL_DOWN    0x42    /* volume down */
-// #define AVRC_ID_MUTE        0x43    /* mute */
-// #define AVRC_ID_PLAY        0x44    /* play */
-// #define AVRC_ID_STOP        0x45    /* stop */
-// #define AVRC_ID_PAUSE       0x46    /* pause */
-// #define AVRC_ID_RECORD      0x47    /* record */
-// #define AVRC_ID_REWIND      0x48    /* rewind */
-// #define AVRC_ID_FAST_FOR    0x49    /* fast forward */
-// #define AVRC_ID_EJECT       0x4A    /* eject */
-// #define AVRC_ID_FORWARD     0x4B    /* forward */
-// #define AVRC_ID_BACKWARD    0x4C    /* backward */
-// #define AVRC_ID_ANGLE       0x50    /* angle */
-// #define AVRC_ID_SUBPICT     0x51    /* subpicture */
-// #define AVRC_ID_F1          0x71    /* F1 */
-// #define AVRC_ID_F2          0x72    /* F2 */
-// #define AVRC_ID_F3          0x73    /* F3 */
-// #define AVRC_ID_F4          0x74    /* F4 */
-// #define AVRC_ID_F5          0x75    /* F5 */
-// #define AVRC_ID_VENDOR      0x7E    /* vendor unique */
-// #define AVRC_KEYPRESSED_RELEASE 0x80
 
 BlootoothA2DSink a2d_sink;
 
@@ -148,7 +90,7 @@ void setup()
   mcp2515.setNormalMode();
 #endif
 
-  // Fix the audio clock
+  // Fix the audio clock, this improves audio quality.
   rtc_clk_apll_enable(1, 15, 8, 5, 6);
   a2d_sink.start("SAAB");
 }
@@ -160,7 +102,6 @@ bool playingState = false;
 String lastContent = "";
 
 long playTime = 0;
-long startPlayTime = millis();
 
 void loop()
 {   
@@ -169,23 +110,14 @@ void loop()
   {
     // Set digital mute ON and do not handle events.
     digitalWrite(mutePin, 1);
+    
     muteState = true;
     playingState = false;
     return;
   }
   else
   {
-    // Ask for key input to send
-    if(Serial.available() > 1)
-    {      
-      String userInput = Serial.readStringUntil('\n');
-      Serial.println(userInput);
-      long val = strtol(userInput.c_str(),NULL,16);      
-      Serial.print("Sending key ");
-      Serial.println(val,DEC);
 
-      esp_avrc_ct_send_passthrough_cmd(6, val, ESP_AVRC_PT_CMD_STATE_PRESSED);
-    }
     // Wait until playing again
     if (playingState)
     {
