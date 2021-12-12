25f8939f944d076c356eea9bc73cbe3ee5363d76
jokubasver <jokubas.ver@gmail.com>
2021-12-12 15:07:49 +0200

    Use LSB I2S format
    According to the UDA1334A datasheet, the chip uses LSB format I2S.
    
    With this I did notice distortion on high volume settings while testing using headphones, but it might be fine on the Saab Aux.

--- esp32_a2dp_receiver/src/main.cpp
+++ esp32_a2dp_receiver/src/main.cpp
@@ -63,7 +63,7 @@ void setup() {
       .sample_rate = 44100,
       .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
       .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
-      .communication_format = (i2s_comm_format_t) (I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
+      .communication_format = (i2s_comm_format_t) (I2S_COMM_FORMAT_STAND_MSB),
       .intr_alloc_flags = 0, // default interrupt priority
       .dma_buf_count = 8,
       .dma_buf_len = 64,
